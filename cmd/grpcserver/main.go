768b44e04cf66723601459940d916b43cc9cd32d
Adalberto Fernandes Júnior <adalberto.fernandesjr@gmail.com>
2024-02-27 21:37:10 -0300

    configurando servidor para inserir os dados

--- cmd/grpcserver/main.go
+++ cmd/grpcserver/main.go
@@ -4,31 +4,31 @@ import (
 	"database/sql"
 	"net"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
 	"github.com/devfullcycle/14-grpc/internal/database"
 	"github.com/devfullcycle/14-grpc/internal/pb"
 	"github.com/devfullcycle/14-grpc/internal/service"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	db, err := sql.Open("sqlite3", "./db.sqlite")
-
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
 
-	categoryDB := database.NewCategory(db)
-	categoryService := service.NewCategoryService(*categoryDB)
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
 
 	grpcServer := grpc.NewServer()
 	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
 	reflection.Register(grpcServer)
 
-	// criando um server e escutando na porta tcp:50051
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":8085")
 	if err != nil {
 		panic(err)
 	}
@@ -36,5 +36,4 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
 	}
-
 }
